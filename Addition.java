d4cf02b3b4b8f6e7a66debd1e1841cec0c89914d
Suba230 <subasundar147@gmail.com>
2022-07-11 10:38:40 +0530

    first commit

---
+++ Addition.java
@@ -0,0 +1,19 @@
+package Day1;
+
+public class Addition {
+	public static void main(String[] args) {
+		int a = 30;
+		int b = 3;
+		int add = a+b;
+		int sub = a-b;
+		int multi =a*b;
+		int division =a/b;
+		int modulus =a%b;
+		System.out.println("the addition of a and b is"+add);
+		System.out.println("the substraction of a and b is" +sub);
+		System.out.println("the multiflication of a and b is"+multi);
+		System.out.println("the division of a and b is "+division);
+		System.out.println("the modulus of a and b is "+modulus);
+	}
+
+}
---
+++ Boolean.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Boolean {
+	public static void main (String[] args) {
+		int a =20;
+		boolean b =(a==0);
+		System.out.println(b);
+				
+	}
+
+}
